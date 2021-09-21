ea483a120ec1ecd2a319fc6200a90112b1120ff3
Alessandra Avelino <80168478+alessandraavelino@users.noreply.github.com>
2021-09-21 16:51:22 -0300

    Add files via upload

---
+++ dataset.js
@@ -0,0 +1,22 @@
+const dataset = [
+    {
+        id: 1,
+        name:"Hamburguer",
+        image:"img/hamburguer.png",
+        descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        preco:"15,00",
+
+    },
+    {   
+        id: 2,
+        name:"Sanduíche",
+        image:"img/hamburguer.png",
+        descricao:"Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        preco:"15,00",
+
+    },
+];
+
+
+export default dataset;
+  
